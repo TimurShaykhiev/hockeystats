5543b7857742ae00ca9ce6cc5e2154e2d7e80df4
Timur Shaykhiev <tshaykhiev@gmail.com>
2016-11-17 23:41:23 +0300

    Bug fix: check end date corrected. Take into account time zones.

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,5 @@
 import MySQLdb as Db
-from datetime import timedelta, date
+from datetime import timedelta, datetime
 
 from logger import create_loader_logger
 from config import CONFIG
@@ -7,6 +7,16 @@ from load import load
 from db_utils.updates import get_last_stat_update, set_last_stat_update
 
 LOG = None
+
+
+# I don't want to do real timezone-aware calculations. Just make sure we don't download game info before
+# midnight in PST. No need in precise time here.
+def _is_in_future(end_date):
+    pst_utc_offset = -8
+    pst_time = datetime.utcnow() + timedelta(hours=pst_utc_offset)
+    end_date = end_date + timedelta(days=1)
+    end_date_midnight = datetime(year=end_date.year, month=end_date.month, day=end_date.day)
+    return end_date_midnight > pst_time
 
 
 def main():
@@ -18,7 +28,7 @@ def main():
             LOG.error('Cannot process. Fix errors from prev load.')
             return
         new_start = new_end = update.end + timedelta(days=1)
-        if new_end > date.today():
+        if _is_in_future(new_end):
             LOG.warning('New date is in the future.')
             return
         successful_load = load(new_start, new_end, db_conn)
